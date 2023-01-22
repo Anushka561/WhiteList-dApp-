af1e248eccacef040aac2b189d238e7f72e37c18
Anushka sahu <117623643+Anushka561@users.noreply.github.com>
2023-01-22 23:19:50 +0530

    Update Whitelist.sol

--- hardhat-tutorial/contracts/Whitelist.sol
+++ hardhat-tutorial/contracts/Whitelist.sol
@@ -11,8 +11,8 @@ contract Whitelist {
     // if an address is whitelisted, we would set it to true, it is false by default for all other addresses.
     mapping(address => bool) public whitelistedAddresses;
 
-    // numAddressesWhitelisted would be used to keep track of how many addresses have been whitelisted
-    // NOTE: Don't change this variable name, as it will be part of verification
+ 
+   
     uint8 public numAddressesWhitelisted;
 
     
